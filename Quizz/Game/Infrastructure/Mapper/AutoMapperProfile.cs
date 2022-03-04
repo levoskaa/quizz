5847f4ead46805f938bb4f2d9fb9751d7cda4674
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-04 13:16:31 +0100

    feat(game): add configurations to infrastructure

---
+++ Quizz/Game/Infrastructure/Mapper/AutoMapperProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using Quizz.Common.ViewModels;
+using Quizz.GameService.Application.Commands;
+using Quizz.GameService.Application.Models;
+using Quizz.GameService.Infrastructure.Exceptions;
+
+namespace Quizz.GameService.Infrastructure.Mapper
+{
+    public class AutoMapperProfile : Profile
+    {
+        public AutoMapperProfile()
+        {
+            CreateMap<CreateGameCommand, Game>();
+
+            CreateMap<GameServiceDomainException, ErrorViewModel>()
+                .ForMember(x => x.Errors, options => options.MapFrom(x => x.ErrorCodes));
+        }
+    }
+}
