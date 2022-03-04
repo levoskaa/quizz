b7488448e47d5bfb3bd4e4409e888e2abe8610bf
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-04 17:55:17 +0100

    fix(game): add missing DI registrations

--- Quizz/Game/Infrastructure/GameServiceModule.cs
+++ Quizz/Game/Infrastructure/GameServiceModule.cs
@@ -1,7 +1,10 @@
 ﻿using Autofac;
+using FluentValidation;
 using MediatR;
+using Quizz.Common.DataAccess;
 using Quizz.Common.Services;
 using Quizz.GameService.Application.Behaviors;
+using Quizz.GameService.Application.Validators;
 using Quizz.GameService.Infrastructure.Repositories;
 
 namespace Quizz.GameService.Infrastructure
@@ -10,18 +13,27 @@ namespace Quizz.GameService.Infrastructure
     {
         protected override void Load(ContainerBuilder builder)
         {
-            builder.RegisterAssemblyTypes(typeof(IGameRepository).Assembly)
-                .Where(type => type.Name.EndsWith("Repository"))
+            // Repositories
+            builder.RegisterAssemblyTypes(typeof(GameRepository).Assembly)
+                .Where(type => type.IsClosedTypeOf(typeof(IRepository<>)))
                 .AsImplementedInterfaces()
                 .InstancePerLifetimeScope();
 
+            // Services
             builder.RegisterType<IdentityService>()
                 .As<IIdentityService>()
                 .InstancePerLifetimeScope();
 
+            // Validators
+            builder.RegisterAssemblyTypes(typeof(CreateGameCommandValidator).Assembly)
+                .Where(type => type.IsClosedTypeOf(typeof(IValidator<>)))
+                .AsImplementedInterfaces()
+                .InstancePerDependency();
+
+            // Behaviors
             builder.RegisterGeneric(typeof(ValidatorBehavior<,>))
                 .As(typeof(IPipelineBehavior<,>))
-                .InstancePerLifetimeScope();
+                .InstancePerDependency();
         }
     }
 }
