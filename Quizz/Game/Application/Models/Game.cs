9d4d0f345134d122602c355f985045b773779764
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-05 13:34:53 +0100

    feat(game): update game entity with question properties

--- Quizz/Game/Application/Models/Game.cs
+++ Quizz/Game/Application/Models/Game.cs
@@ -1,5 +1,7 @@
 ﻿using Quizz.Common.DataAccess;
 using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Quizz.GameService.Application.Models
 {
@@ -10,5 +12,32 @@ namespace Quizz.GameService.Application.Models
         public string OwnerId { get; set; }
 
         public DateTime UpdatedAt { get; set; }
+
+        private readonly List<GameQuestion> gameQuestions;
+
+        public IReadOnlyCollection<Question> Questions
+        {
+            get
+            {
+                return gameQuestions.Select(x => x.Question)
+                    .ToList()
+                    .AsReadOnly();
+            }
+        }
+
+        public IReadOnlyCollection<string> QuestionIds
+        {
+            get
+            {
+                return gameQuestions.Select(x => x.QuestionId)
+                    .ToList()
+                    .AsReadOnly();
+            }
+        }
+
+        public Game()
+        {
+            gameQuestions = new List<GameQuestion>();
+        }
     }
 }
---
+++ Quizz/Game/Application/Models/GameQuestion.cs
@@ -0,0 +1,11 @@
+﻿namespace Quizz.GameService.Application.Models
+{
+    public class GameQuestion
+    {
+        public int GameId { get; set; }
+        public Game Game { get; set; }
+
+        public string QuestionId { get; set; }
+        public Question Question { get; set; }
+    }
+}
