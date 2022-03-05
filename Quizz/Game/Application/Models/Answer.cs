fd3b719eb076dfd2e894a46793873bb0ffe94dd1
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-05 15:35:25 +0100

    feat(game): add answer entity

---
+++ Quizz/Game/Application/Models/Answer.cs
@@ -0,0 +1,11 @@
+﻿using Quizz.Common.DataAccess;
+
+namespace Quizz.GameService.Application.Models
+{
+    public class Answer : Entity<int>
+    {
+        public string Text { get; set; }
+
+        public string QuestionId { get; set; }
+    }
+}
