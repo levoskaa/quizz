f8d24ab566e1b15cadd584905f13d84ce9d551d1
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-05 20:39:03 +0100

    feat(game): add attributes to endpoint

--- Quizz/Game/Controllers/GamesController.cs
+++ Quizz/Game/Controllers/GamesController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Quizz.Common.ViewModels;
 using Quizz.GameService.Application.Commands;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Quizz.GameService.Controllers
@@ -10,19 +12,25 @@ namespace Quizz.GameService.Controllers
     [Route("api/[controller]")]
     [ApiController]
     [Authorize]
+    [Produces("application/json")]
     public class GamesController : ControllerBase
     {
         private readonly IMediator mediator;
+        private readonly IHttpContextAccessor contextAccessor;
 
-        public GamesController(IMediator mediator)
+        public GamesController(IMediator mediator, IHttpContextAccessor contextAccessor)
         {
             this.mediator = mediator;
+            this.contextAccessor = contextAccessor;
         }
 
         [HttpPost]
+        [ProducesResponseType(typeof(EntityCreatedViewModel<int>), (int)HttpStatusCode.Created)]
+        [ProducesResponseType(typeof(ErrorViewModel), (int)HttpStatusCode.BadRequest)]
         public async Task<EntityCreatedViewModel<int>> CreateGame([FromBody] CreateGameCommand createGameCommand)
         {
             var createdId = await mediator.Send(createGameCommand);
+            contextAccessor.HttpContext.Response.StatusCode = (int)HttpStatusCode.Created;
             return new EntityCreatedViewModel<int>
             {
                 Id = createdId
