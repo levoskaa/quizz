306f8fdbcbbaf545cbabe6222936a751fe08d64b
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-09 17:52:45 +0100

    feat(*): add layout container

--- Quizz/quizz.web-client/src/app/app-routing.module.ts
+++ Quizz/quizz.web-client/src/app/app-routing.module.ts
@@ -2,10 +2,17 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { SigninCallbackComponent } from './core/auth/signin-callback/signin-callback.component';
 import { SignoutCallbackComponent } from './core/auth/signout-callback/signout-callback.component';
+import { LayoutContainerComponent } from './shared/components/layout/layout-container/layout-container.component';
 
 const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: LayoutContainerComponent,
+  },
   { path: 'signin-callback', component: SigninCallbackComponent },
   { path: 'signout-callback', component: SignoutCallbackComponent },
+  { path: '**', redirectTo: '' },
 ];
 
 @NgModule({
---
+++ Quizz/quizz.web-client/src/app/shared/components/layout/layout-container/layout-container.component.ts
@@ -0,0 +1,6 @@
+import { Component } from '@angular/core';
+
+@Component({
+  templateUrl: './layout-container.component.html',
+})
+export class LayoutContainerComponent {}
