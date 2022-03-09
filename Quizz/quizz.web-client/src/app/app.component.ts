51e9cc435d9f563f63ca14161095c9e3413b88f5
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-09 21:12:58 +0100

    refactor(*): simplify app component

--- Quizz/quizz.web-client/src/app/app.component.ts
+++ Quizz/quizz.web-client/src/app/app.component.ts
@@ -3,9 +3,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 
 @Component({
-  selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'quizz-web-client';
