ed48ceced7d023d4a453c00e80fd9d8803cb23ca
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-09 21:30:30 +0100

    fix(*): add selector to app component

--- Quizz/quizz.web-client/src/app/app.component.ts
+++ Quizz/quizz.web-client/src/app/app.component.ts
@@ -3,6 +3,7 @@ import { Component } from '@angular/core';
 import { AuthService } from './core/services/auth.service';
 
 @Component({
+  selector: 'app-root',
   templateUrl: './app.component.html',
 })
 export class AppComponent {
