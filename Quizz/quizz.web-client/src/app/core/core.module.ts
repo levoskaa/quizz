45937172571c29dd5e7126a2f612078e2577b809
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-09 21:17:23 +0100

    refactor(*): update module configurations

--- Quizz/quizz.web-client/src/app/core/core.module.ts
+++ Quizz/quizz.web-client/src/app/core/core.module.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SigninCallbackComponent } from './auth/signin-callback/signin-callback.component';
 import { SignoutCallbackComponent } from './auth/signout-callback/signout-callback.component';
 import { TokenInterceptor } from './interceptors/token.interceptor';
@@ -9,5 +11,6 @@ import { TokenInterceptor } from './interceptors/token.interceptor';
   declarations: [SigninCallbackComponent, SignoutCallbackComponent],
   providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }],
   imports: [CommonModule],
+  exports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
 })
 export class CoreModule {}
--- Quizz/quizz.web-client/src/app/shared/shared.module.ts
+++ Quizz/quizz.web-client/src/app/shared/shared.module.ts
@@ -1,8 +1,20 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { RouterModule } from '@angular/router';
+import { TranslateModule } from '@ngx-translate/core';
+import { NgScrollbarModule } from 'ngx-scrollbar';
+import { HeaderComponent } from './components/layout/header/header.component';
+import { LayoutContainerComponent } from './components/layout/layout-container/layout-container.component';
+
+const components = [LayoutContainerComponent, HeaderComponent];
+
+const commonModules = [MatToolbarModule, MatSidenavModule, NgScrollbarModule, TranslateModule];
 
 @NgModule({
-  declarations: [],
-  imports: [CommonModule],
+  declarations: [...components],
+  imports: [CommonModule, RouterModule, ...commonModules],
+  exports: [...commonModules],
 })
 export class SharedModule {}
