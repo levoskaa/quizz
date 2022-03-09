49ffb2654372e73c74ab764fb86af107de2ca2b4
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-09 16:00:44 +0100

    feat(*): install Angular Material

--- Quizz/quizz.web-client/src/app/app.module.ts
+++ Quizz/quizz.web-client/src/app/app.module.ts
@@ -1,13 +1,14 @@
 import { HttpClientModule } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [CoreModule, BrowserModule, AppRoutingModule, HttpClientModule],
+  imports: [CoreModule, BrowserModule, AppRoutingModule, HttpClientModule, BrowserAnimationsModule],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
