bc3bc6ef9130f3237b783952e7f2b3fc365b1fbe
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-10 13:18:59 +0100

    feat(*): add localization support

--- Quizz/quizz.web-client/src/app/core/core.module.ts
+++ Quizz/quizz.web-client/src/app/core/core.module.ts
@@ -1,16 +1,34 @@
 import { CommonModule } from '@angular/common';
-import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { SigninCallbackComponent } from './auth/signin-callback/signin-callback.component';
 import { SignoutCallbackComponent } from './auth/signout-callback/signout-callback.component';
+import { TRANSLATE_INITIALIZER } from './initializers/translate.initializer';
 import { TokenInterceptor } from './interceptors/token.interceptor';
+
+const appInitializers = [TRANSLATE_INITIALIZER];
+
+function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http);
+}
 
 @NgModule({
   declarations: [SigninCallbackComponent, SignoutCallbackComponent],
-  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }],
-  imports: [CommonModule],
+  providers: [{ provide: HTTP_INTERCEPTORS, useClass: TokenInterceptor, multi: true }, ...appInitializers],
+  imports: [
+    CommonModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps: [HttpClient],
+      },
+    }),
+  ],
   exports: [BrowserModule, HttpClientModule, BrowserAnimationsModule],
 })
 export class CoreModule {}
