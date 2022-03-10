7e7bc0262a569022c16ea1e069b8dc1a8f1a456b
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-03-10 12:15:38 +0100

    feat(*): create layout skeleton

--- Quizz/quizz.web-client/src/app/shared/components/layout/layout-container/layout-container.component.ts
+++ Quizz/quizz.web-client/src/app/shared/components/layout/layout-container/layout-container.component.ts
@@ -2,5 +2,6 @@ import { Component } from '@angular/core';
 
 @Component({
   templateUrl: './layout-container.component.html',
+  styleUrls: ['./layout-container.component.scss'],
 })
 export class LayoutContainerComponent {}
