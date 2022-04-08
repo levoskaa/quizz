e93ac922f519d5966ffe006a77dd4ef0055f4b65
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-04-08 20:25:10 +0200

    feat(*): add integration event logging

---
+++ Quizz/BuildingBlocks/EventBus/IntegrationEventLog/IntegrationEventLogContext.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Quizz.BuildingBlocks.IntegrationEventLog;
+
+public class IntegrationEventLogContext : DbContext
+{
+    public DbSet<IntegrationEventLogEntry> IntegrationEventLogs { get; set; }
+
+    public IntegrationEventLogContext(DbContextOptions<IntegrationEventLogContext> options)
+        : base(options)
+    { }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        builder.Entity<IntegrationEventLogEntry>(ConfigureIntegrationEventLogEntry);
+    }
+
+    private void ConfigureIntegrationEventLogEntry(EntityTypeBuilder<IntegrationEventLogEntry> builder)
+    {
+        builder.ToTable("IntegrationEventLog");
+
+        builder.HasKey(e => e.EventId);
+
+        builder.Property(e => e.EventId)
+            .IsRequired();
+
+        builder.Property(e => e.Content)
+            .IsRequired();
+
+        builder.Property(e => e.CreationTime)
+            .IsRequired();
+
+        builder.Property(e => e.State)
+            .IsRequired();
+
+        builder.Property(e => e.TimesSent)
+            .IsRequired();
+
+        builder.Property(e => e.EventTypeName)
+            .IsRequired();
+    }
+}
