893aae100d842eb2bd2ed9f406324ff2837d28ce
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-04-16 21:59:28 +0200

    feat(questions): generate multiple choice question component

---
+++ Quizz/quizz-web-client/src/app/features/game/components/multiple-choice-question/multiple-choice-question.component.ts
@@ -0,0 +1,7 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-multiple-choice-question',
+  templateUrl: './multiple-choice-question.component.html',
+})
+export class MultipleChoiceQuestionComponent {}
--- Quizz/quizz-web-client/src/app/features/game/game.module.ts
+++ Quizz/quizz-web-client/src/app/features/game/game.module.ts
@@ -4,6 +4,7 @@ import { SharedModule } from 'src/app/shared/shared.module';
 import { NewGameDialogComponent } from './components/dialogs/new-game-dialog/new-game-dialog.component';
 import { GameFormComponent } from './components/forms/game-form/game-form.component';
 import { GameCardComponent } from './components/game-card/game-card.component';
+import { MultipleChoiceQuestionComponent } from './components/multiple-choice-question/multiple-choice-question.component';
 import { QuestionsListComponent } from './components/questions-list/questions-list.component';
 import { GameRoutingModule } from './game-routing.module';
 import { GameDetailPageComponent } from './pages/game-detail-page/game-detail-page.component';
@@ -17,6 +18,7 @@ import { GamesPageComponent } from './pages/games-page/games-page.component';
     NewGameDialogComponent,
     GameFormComponent,
     QuestionsListComponent,
+    MultipleChoiceQuestionComponent,
   ],
   imports: [CommonModule, GameRoutingModule, SharedModule],
 })
