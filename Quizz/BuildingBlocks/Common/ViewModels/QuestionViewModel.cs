d008a59d82fc439da1e03c505f2ce8593de899f6
levoskaa <47329402+levoskaa@users.noreply.github.com>
2022-04-16 16:48:02 +0200

    refactor(questions): add required attribute to viewmodel properties

--- Quizz/BuildingBlocks/Common/ViewModels/QuestionViewModel.cs
+++ Quizz/BuildingBlocks/Common/ViewModels/QuestionViewModel.cs
@@ -1,13 +1,19 @@
 ﻿using Quizz.Common.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quizz.Common.ViewModels;
 
 public class QuestionViewModel
 {
+    [Required]
     public string Id { get; init; }
+    [Required]
     public string Text { get; init; }
+    [Required]
     public QuestionType Type { get; init; }
+    [Required]
     public int Index { get; init; }
+    [Required]
     public int TimeLimitInSeconds { get; init; }
     public Answer AnswerPossibilites { get; init; }
     public int CorrectIdOrder { get; init; }
--- Quizz/BuildingBlocks/Common/ViewModels/QuestionsListViewModel.cs
+++ Quizz/BuildingBlocks/Common/ViewModels/QuestionsListViewModel.cs
@@ -1,8 +1,10 @@
 ﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace Quizz.Common.ViewModels;
 
 public class QuestionsListViewModel
 {
+    [Required]
     public IEnumerable<QuestionViewModel> Data { get; init; }
 }
