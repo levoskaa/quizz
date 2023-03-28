9998179983a0e1ced19655ebcf47f621a2cf98cb
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-03-28 19:55:28 +0200

    feat: implement true or false answering

--- Quizz/quizz-web-client/src/app/features/quiz-runner/components/true-or-false-answers/true-or-false-answers.component.ts
+++ Quizz/quizz-web-client/src/app/features/quiz-runner/components/true-or-false-answers/true-or-false-answers.component.ts
@@ -1,7 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { OperationMode } from '../../models/quiz-runner.models';
+import { QuizRunnerService } from '../../services/quiz-runner.service';
 
 @Component({
   selector: 'app-true-or-false-answers',
   templateUrl: './true-or-false-answers.component.html',
+  styleUrls: ['./true-or-false-answers.component.scss'],
 })
-export class TrueOrFalseAnswersComponent {}
+export class TrueOrFalseAnswersComponent {
+  @Input() mode: OperationMode = 'control';
+  @Output() answered = new EventEmitter<boolean>();
+
+  constructor(private readonly quizRunner: QuizRunnerService) {}
+
+  submit(answer: boolean): void {
+    this.quizRunner.answerQuestion(answer).then((result) => this.answered.emit(result));
+  }
+}
