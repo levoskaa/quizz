91d5994eb4ada6faa3889c2c14e296176f4b7edf
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-03-28 17:02:38 +0200

    feat(client): implement multiple choice answering

--- Quizz/quizz-web-client/src/app/features/quiz-runner/components/multiple-choice-answers/multiple-choice-answers.component.ts
+++ Quizz/quizz-web-client/src/app/features/quiz-runner/components/multiple-choice-answers/multiple-choice-answers.component.ts
@@ -1,10 +1,28 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { AnswerViewModel } from '../../../../shared/models/generated/game-generated.models';
+import { OperationMode } from '../../models/quiz-runner.models';
 
 @Component({
   selector: 'app-multiple-choice-answers',
   templateUrl: './multiple-choice-answers.component.html',
+  styleUrls: ['./multiple-choice-answers.component.scss'],
 })
 export class MultipleChoiceAnswersComponent {
   @Input() answers: AnswerViewModel[];
+  @Input() mode: OperationMode = 'control';
+  @Output() answered = new EventEmitter<void>();
+  selected = Array.from({ length: 4 }, () => false);
+
+  submit(): void {
+    // TODO
+    this.answered.emit();
+  }
+
+  getAnswerBoxClasses(index: number): object {
+    return { [`color-${index + 1}`]: true, selected: this.selected[index] };
+  }
+
+  toggleSelect(index: number): void {
+    this.selected[index] = !this.selected[index];
+  }
 }
---
+++ Quizz/quizz-web-client/src/app/features/quiz-runner/pages/quiz-answering-page/quiz-answering-page.component.ts
@@ -0,0 +1,39 @@
+import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngxs/store';
+import { Observable } from 'rxjs';
+import { tap } from 'rxjs/operators';
+import { filterNullAndUndefined } from '../../../../core/operators/filterNullAndUndefined';
+import { UnsubscribeOnDestroy } from '../../../../shared/classes/unsubscribe-on-destroy';
+import {
+  QuestionType,
+  QuestionViewModel,
+} from '../../../../shared/models/generated/game-generated.models';
+import { QuizRunnerService } from '../../services/quiz-runner.service';
+import { QuizRunnerState } from '../../states/quiz-runner.state';
+
+@Component({
+  templateUrl: './quiz-answering-page.component.html',
+  styleUrls: ['./quiz-answering-page.component.scss'],
+})
+export class QuizAnsweringPageComponent extends UnsubscribeOnDestroy implements OnInit {
+  gameStarted = false;
+  questionAnswered = false;
+  question$: Observable<QuestionViewModel>;
+  QuestionType = QuestionType;
+
+  constructor(private readonly quizRunner: QuizRunnerService, private readonly store: Store) {
+    super();
+  }
+
+  ngOnInit(): void {
+    this.subscribe(this.quizRunner.gameStarted$.pipe(tap(() => (this.gameStarted = true))));
+    this.question$ = this.store.select(QuizRunnerState.currentQuestion).pipe(
+      filterNullAndUndefined(),
+      tap(() => (this.questionAnswered = false))
+    );
+  }
+
+  onAnswered(): void {
+    this.questionAnswered = true;
+  }
+}
