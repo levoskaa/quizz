0be7925cf491bc9fbe47aec09240ba32c99de595
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-04-13 15:22:47 +0200

    feat(api): add missing command validators

---
+++ Quizz/SignalR/Application/Validators/InitGameCommandValidator.cs
@@ -0,0 +1,18 @@
+﻿using FluentValidation;
+using Quizz.Common.ErrorHandling;
+using Quizz.SignalR.Application.Commands;
+
+namespace Quizz.SignalR.Application.Validators
+{
+    public class InitGameCommandValidator : AbstractValidator<InitGameCommand>
+    {
+        public InitGameCommandValidator()
+        {
+            RuleFor(command => command.UserId).NotEmpty()
+                .WithMessage(ValidationError.UserIdRequired);
+
+            RuleFor(command => command.GameId).NotEmpty()
+                .WithMessage(ValidationError.GameIdRequired);
+        }
+    }
+}
