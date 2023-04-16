932e1a0297a2133a4c30a7bc2ab446f34f55bffb
levoskaa <47329402+levoskaa@users.noreply.github.com>
2023-04-16 20:50:11 +0200

    test: add test for game question modified event handler

---
+++ Quizz/GameService.UnitTests/Application/DomainEvents/SetGameModificationTimeWhenGameQuestionsUpdatedDomainEventHandlerTests.cs
@@ -0,0 +1,37 @@
+﻿using Quizz.GameService.Application.DomainEvents;
+using Quizz.GameService.Application.Models;
+
+namespace GameService.UnitTests.Application.DomainEvents
+{
+    public class SetGameModificationTimeWhenGameQuestionsUpdatedDomainEventHandlerTests
+    {
+        [Fact]
+        public async Task Handle_ShouldWork()
+        {
+            // Arrange
+            var game = GetSampleGame();
+            var notification = GetSampleNotification(game);
+            var eventHandler = new SetGameModificationTimeWhenGameQuestionsUpdatedDomainEventHandler();
+
+            // Act
+            await eventHandler.Handle(notification, default);
+
+            // Assert
+            Assert.Equal(notification.UpdatedAt, game.UpdatedAt);
+        }
+
+        private Game GetSampleGame()
+        {
+            return new Game
+            {
+                UpdatedAt = new DateTime(2000, 2, 3, 11, 21, 9),
+            };
+        }
+
+        private GameQuestionsUpdatedDomainEvent GetSampleNotification(Game game)
+        {
+            var updateTime = new DateTime(2019, 9, 4, 21, 49, 12);
+            return new GameQuestionsUpdatedDomainEvent(game, updateTime);
+        }
+    }
+}
